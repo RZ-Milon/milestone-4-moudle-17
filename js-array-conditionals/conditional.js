d6ce74320b892fdbee0eed6c49155d7b2a929c2d
RZMilon <rzmilon@gmail.com>
2022-08-01 01:37:43 +0600

    conditional

---
+++ js-array-conditionals/conditional.js
@@ -0,0 +1,46 @@
+var iphonePrice = 80000;
+var myBudget = 95000;
+
+/**
+ * if iphone price is less than my budget
+ *      i will buy the iphone
+ * 
+ * 
+ * if (condition){
+ *   // will excute if the condition is t
+ * } 
+ * 
+ * 
+**/ 
+
+
+
+if (iphonePrice < myBudget){
+    console.log('buy iphone');
+}
+
+var chickenPrice = 500;
+var myMony = 50;
+// if (chickenPrice <= myMony){
+//     console.log('murgi khabo');
+// }
+
+// if (chickenPrice > myMony){
+//     console.log('Khamu dal vorta');
+// }
+
+
+/**
+ * if chicken price is lss than may budget
+ * i will eat chicken
+ * 
+ * na hoi : else 
+ * 
+*/ 
+
+if(chickenPrice < myMony){
+    console.log('i will eat chicken');
+}
+else{
+    console.log('i will eat popcorn');
+}
