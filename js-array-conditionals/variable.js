f642c3ac244e4d9b0e8d825ca31dd898fb1fcda3
RZMilon <rzmilon@gmail.com>
2022-07-31 23:00:50 +0600

    variable add

---
+++ js-array-conditionals/variable.js
@@ -0,0 +1,30 @@
+// number type variable
+var orangePrice = 100;
+
+// string type variable
+var name = "karim uddin";
+
+// boolian type variable
+var honeyIsSweet = true;
+
+// two types of number: Integer: 120, & Float, Decimal: 3.5,
+
+// variable naming convention : rules, best practice
+/**
+ * 1. single word
+ * 2. no gap or space
+ * 3. no quote
+ * 4. no keyword
+ * 5. can not start with a number
+ * 6. can not contain dash
+ * 7. should use camelCase 
+*/
+
+
+// Math Operation: +, -, *, /
+// Shorthand: +=, -=, ++, --
+// covert: parseInt, parseFloat
+
+// toFixed
+
+// reminder: %, vagshes
